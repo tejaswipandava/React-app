cb8a7d351cca38f4c2f8b5e4a8d131f00f725836
Tejaswi Pandava <tejaswi.pandava@gmail.com>
2023-09-11 23:29:47 +0530

    fix styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Message from "./Message";
 
-function App(){
-  return <div><Message /></div>
+function App() {
+  return (
+    <div>
+      <Message />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,12 +1,11 @@
-
 //pascal casing: naming convention for function components
 /* Component are of 2 types 
     1 class components 
     2 functional components : these are popular for being easy and less code to write
 */
-function Message(){
-    const name = "Teja"
-    return <h1>Hello {name}</h1>
+function Message() {
+  const name = "Teja";
+  return <h1>Hello {name}</h1>;
 }
 
 export default Message;
