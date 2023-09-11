2f4505de8027b2a01910f3a748741b4840c5b133
Tejaswi Pandava <tejaswipandava@Tejaswis-MacBook-Pro-2.local>
2023-09-11 22:33:38 +0530

    C-1 | tejaswi | adding message component

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+//pascal casing: naming convention for function components
+/* Component are of 2 types 
+    1 class components 
+    2 functional components : these are popular for being easy and less code to write
+*/
+function Message(){
+    return <h1>Hello World</h1>
+}
+
+export default Message;
