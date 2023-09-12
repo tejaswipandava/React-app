0f732d7c1e62cdb610c21aed995b1a69e615cc5c
Tejaswi Pandava <tejaswi.pandava@gmail.com>
2023-09-12 15:48:47 +0530

    conditional rendering 2 inline complicated expressions

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,18 +1,11 @@
 function ListGroup() {
   var items = ["egg", "chicken", "rice", "vegetables", "pasta", "pizza"];
   items = [];
-  //problem with this approach is we will loose the h1 tag
-  if (items.length === 0)
-    return (
-      <>
-        <h1>We have 2 elements</h1>
-        <p>No elements found</p>
-      </>
-    );
 
   return (
     <>
       <h1>We have 2 elements</h1>
+      {items.length === 0 ? <p>No element found</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>
