7283306092c6ece2645129bd55689a340de6cb9d
Tejaswi Pandava <tejaswi.pandava@gmail.com>
2023-09-12 16:34:55 +0530

    handling click  events

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,8 +1,7 @@
 function ListGroup() {
   var items = ["egg", "chicken", "rice", "vegetables", "pasta", "pizza"];
-  items = [];
 
-  var getMessage = () => {
+  const getMessage = () => {
     return items.length === 0 ? <p>No element found</p> : null;
   };
 
@@ -11,8 +10,12 @@ function ListGroup() {
       <h1>We have 2 elements</h1>
       {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(item, index)}
+          >
             {item}
           </li>
         ))}
