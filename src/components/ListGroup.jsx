426b9533cafac25518bc6fb8e780a059a2f5ea92
Tejaswi Pandava <tejaswi.pandava@gmail.com>
2023-09-12 15:52:01 +0530

    conditional rendering 3 using variable and itenary operator with a risky null

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -2,10 +2,12 @@ function ListGroup() {
   var items = ["egg", "chicken", "rice", "vegetables", "pasta", "pizza"];
   items = [];
 
+  var message = items.length === 0 ? <p>No element found</p> : null;
+
   return (
     <>
       <h1>We have 2 elements</h1>
-      {items.length === 0 ? <p>No element found</p> : null}
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>
